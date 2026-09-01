252c44123b4a9408e9fa41c6b80e285f3a800eb0
Phil-osophyx <akshagarwalpro@gmail.com>
2026-09-01 21:49:45 +0530

    Add Songs

---
+++ app.js
@@ -0,0 +1,27 @@
+const songs= [
+     {
+        name: "Dancing Queen",
+        artist: "ABBA",
+        file: "songs/dancingqueen.mp3",
+        image: "images/dancingqueen.jpg"
+    },
+    {
+        name: "The Winner Takes It All",
+        artist: "ABBA",
+        file:"songs/TheWinner.mp3",
+        image:"images/the-winner.png"
+    },
+     {
+        name: "Hey Jude",
+        artist: "The Beatles",
+        file: "songs/heyjude.mp3",
+        image: "images/heyjude.jpg"
+    },
+
+    {
+        name: "Bohemian Rhapsody",
+        artist: "The Queen",
+        file: "songs/Bohemain.mp3",
+        image: "images/bohemian-rhapsody.jpg"
+    },
+]
